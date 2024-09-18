f02752232c4396f4198b1b6aa7427db6d5574528
Haribabu05 <sharibabu507@gmail.com>
2024-09-18 17:38:20 +0530

    chatbot source code

---
+++ file3.py
@@ -0,0 +1,23 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st
+
+st.title("Abhinav's Show Chat bot")
+input_txt = st.text_input("Please enter your queries here...")
+
+prompt = ChatPromptTemplate.from_messages(
+    [("system","you are a helpful AI assistant. Your name is Karthik's Assistant"),
+      ("user", "user query:{query}")
+      ])
+
+llm = Ollama(model="llama2")
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_txt:
+    st.write(chain.invoke({"query":input_txt}))
+
+
+
+
